18a6d91821784787ae954fa6cd3d70e21e7f9f0b
AnSC <berg123@naver.com>
2023-06-15 17:56:45 +0900

    quiz - double check improvement

--- C/double_check.c
+++ C/double_check.c
@@ -3,15 +3,10 @@
 
 int main(void) {
     char name[BUFFER_SIZE_MAX] = {0,};
-    int i = 0;
 
-    scanf("%s", &name);
-    for(i = 0; i < BUFFER_SIZE_MAX; i++) {
-        if(name[i] == 0) {
-            break;
-        }
-        printf("%s",name[i]);
-    }
-
+    scanf("%s", name); 
+    printf("%s",name); 
     printf("??!");
+
+    return 0;
 }
