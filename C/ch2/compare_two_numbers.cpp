9ff0231ebb8f737c9b8e0207bc65992d88ba1fd9
AnSC <berg123@naver.com>
2023-06-23 17:33:06 +0900

    quiz - compare two numbers

---
+++ C/ch2/compare_two_numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main(void) {
+    int a, b;
+    
+    std::cin >> a >> b;
+    
+    if (a < b)          std::cout << "<" << std::endl;
+    else if (a > b)     std::cout << ">" << std::endl;
+    else if (a == b)    std::cout << "==" << std::endl;
+
+    return 0;
+}
