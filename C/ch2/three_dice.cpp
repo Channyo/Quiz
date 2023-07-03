06cc935a6f49815a057ce4446eeeb313fde9ffed
AnSC <berg123@naver.com>
2023-07-03 18:04:15 +0900

    quiz - three dice

---
+++ C/ch2/three_dice.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+int main(void) {
+    int dice[3], price;
+
+    std::cin >> dice[0] >> dice[1] >> dice[2];
+
+    if((dice[0] == dice[1]) && (dice[1] == dice[2])) {
+        price = 10000 + (dice[0] * 1000);
+    }
+    else if((dice[0] != dice[1]) && (dice[1] != dice[2]) && (dice[2] != dice[0])) {
+        
+        if(dice[0] > dice[1]) {
+            
+            if(dice[0] > dice[2])
+                price = dice[0];
+            else
+                price = dice[2];
+        }
+        else {
+            
+            if(dice[1] > dice[2])
+                price = dice[1];
+            else
+                price = dice[2];
+        }
+        price *= 100;
+    }
+    else {
+        
+        if(dice[0] == dice[1])
+            price = 1000 + (dice[0] * 100);
+        else if(dice[1] == dice[2])
+            price = 1000 + (dice[1] * 100);
+        else if(dice[2] == dice[0])
+            price = 1000 + (dice[2] * 100);
+    }
+
+    std::cout << price << std::endl;
+
+    return 0;
+}
