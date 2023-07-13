de535a8610460f1ecd0ed0fbd26a824e2a69c474
AnSC <berg123@naver.com>
2023-07-13 18:15:20 +0900

    quiz - fast addition correct

--- C/ch3/fast_addition.c
+++ C/ch3/fast_addition.c
@@ -3,21 +3,13 @@
 
 int main(void) {
     int T, i;
-    int *a, *b;
+    int a, b;
 
-    a = (int *)malloc(T * sizeof(int));
-    b = (int *)malloc(T * sizeof(int));
     scanf("%d", &T);
     for(i = 0; i < T; i++) {
-        scanf("%d %d", &a[i], &b[i]);
+        scanf("%d %d", &a, &b);
+        printf("%d\n", (a + b));
     }
-
-    for(i = 0; i < T; i++) {
-        printf("%d\n", (a[i] + b[i]));
-    }
-
-    free(a);
-    free(b);
 
     return 0;
 
