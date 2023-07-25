34cca36117d92e9a181df664d6ab2626280b0bcb
AnSC <berg123@naver.com>
2023-07-25 17:43:45 +0900

    quiz - Add EOF addition c++

---
+++ C/ch3/EOF_addition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using std::cin;
+using std::cout;
+
+int main(void) {
+
+    int a, b;
+
+    while((cin >> a >> b)) {
+        cout << a+b << "\n";
+    }
+
+    return 0;
+}
