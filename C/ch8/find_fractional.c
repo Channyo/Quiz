0a6a43d7f7bf8138ed5d9e430ca02ca28fe591b9
Channyo <berg123@naver.com>
2023-10-23 18:26:58 +0900

    quiz - find fractional -ing

---
+++ C/ch8/find_fractional.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+enum {
+    vertical_low = 0,
+    horizontal_low,
+    vertical_plus,
+    horizontal_plus,
+};
+
+int main(void) {
+    long x;
+    int vertical = 1, horizontal = 1, state = 0;
+
+    scanf("%ld", &x);
+
+    switch(state) {
+        case vertical_low : 
+            break;
+
+        case horizontal_low :
+            break;
+
+        case vertical_plus :
+            break;
+
+        case horizontal_plus :
+            break;
+    }
+
+
+}
