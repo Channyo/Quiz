6f6cd40c4e48c419d6e1108446323e3e754db6c9
Channyo <berg123@naver.com>
2023-10-24 20:08:56 +0900

    quiz - find fractional

--- C/ch8/find_fractional.c
+++ C/ch8/find_fractional.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#if 0
 enum {
     vertical_low = 0,
     horizontal_low,
@@ -13,19 +13,56 @@ int main(void) {
 
     scanf("%ld", &x);
 
-    switch(state) {
-        case vertical_low : 
-            break;
+    while(x > 1) {
+        x--;
+        switch(state) {
+            case vertical_low : 
+                horizontal++;
+                state = vertical_plus;
+                break;
 
-        case horizontal_low :
-            break;
+            case horizontal_low :
+                vertical++;
+                state = horizontal_plus;
+                break;
 
-        case vertical_plus :
-            break;
+            case vertical_plus :
+                horizontal--;
+                vertical++;
+                if(horizontal == 1) {
+                    state = horizontal_low;
+                }
+                break;
 
-        case horizontal_plus :
-            break;
+            case horizontal_plus :
+                vertical--;
+                horizontal++;
+                if(vertical == 1) {
+                    state = vertical_low;
+                }
+                break;
+        }
+    }
+    printf("%d/%d", vertical, horizontal);
+
+    return 0;
+}
+#else
+int main(void) {
+    int x, n = 1;
+    
+    scanf("%d", &x);
+    
+    while(x > n) {
+        x -= n;
+        n++;
     }
 
-
+    if((n % 2) == 0)
+        printf("%d/%d", x, n-x + 1);
+    else
+        printf("%d/%d", n-x+1, x );
+    
+    return 0;
 }
+#endif
